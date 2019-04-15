e49fcf47868189fca25511297a81794ce8810df8
Dmytry Shvaika <shvajka@gmail.com>
2019-04-15 15:39:37 +0300

    fix error handling in planner

--- planner/src/app.js
+++ planner/src/app.js
@@ -6,37 +6,41 @@ import { Job } from './models';
 const plannerLogger = logger.getLogger('PlanerApp');
 
 const pushToQueue = async () => {
-  const jobs = await Job.find({
-    status: 'ACTIVE'
-  });
-
-  // eslint-disable-next-line
-  for (let job of jobs) {
-    const notification = {
-      jobId: job._id.toString()
-    };
-
-    if (
-      moment(job.departureDate, 'YYYY-MM-DD')
-        .tz('Europe/Kiev')
-        .add(1, 'day')
-        .set('hours', 0)
-        .diff(moment().tz('Europe/Kiev'), 'hours', true) < 3
-    ) {
-      // eslint-disable-next-line
-      await job.markAsExpired();
-
-      notification.type = 'EXPIRATION';
-    }
+  try {
+    const jobs = await Job.find({
+      status: 'ACTIVE'
+    });
 
     // eslint-disable-next-line
-    await queue.publish(
-      notification.type === 'EXPIRATION'
-        ? process.env.NOTIFICATIONS_QUEUE
-        : process.env.WORKER_QUEUE,
-      'fanout',
-      JSON.stringify(notification)
-    );
+    for (let job of jobs) {
+      const notification = {
+        jobId: job._id.toString()
+      };
+
+      if (
+        moment(job.departureDate, 'YYYY-MM-DD')
+          .tz('Europe/Kiev')
+          .add(1, 'day')
+          .set('hours', 0)
+          .diff(moment().tz('Europe/Kiev'), 'hours', true) < 3
+      ) {
+        // eslint-disable-next-line
+        await job.markAsExpired();
+
+        notification.type = 'EXPIRATION';
+      }
+
+      // eslint-disable-next-line
+      await queue.publish(
+        notification.type === 'EXPIRATION'
+          ? process.env.NOTIFICATIONS_QUEUE
+          : process.env.WORKER_QUEUE,
+        'fanout',
+        JSON.stringify(notification)
+      );
+    }
+  } catch (error) {
+    plannerLogger.error(error);
   }
 };
 
