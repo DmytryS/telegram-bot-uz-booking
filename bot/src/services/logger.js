aede0a2c1edbba18e228c8a73b2e4d5732157750
Dmytry Shvaika <shvajka@gmail.com>
2019-06-09 00:58:38 +0300

    added yml configs

--- bot/src/services/logger.js
+++ bot/src/services/logger.js
@@ -1,27 +1,19 @@
 import log4js from 'log4js';
-import fs from 'fs';
 
-const pathToFile = `${process.cwd()}/logs`;
-
-if (!fs.existsSync(pathToFile)) {
-  fs.mkdirSync(pathToFile);
-}
 const appenders = {
-  file: {
-    type: 'file',
-    filename: `${process.cwd()}/logs/${process.env.NODE_ENV}-app.log`,
-    timezoneOffset: 0
+  console: {
+    type: 'console'
   }
 };
 const categories = {
-  default: { appenders: ['file'], level: 'info' }
+  default: {
+    appenders: ['console'],
+    level: 'info'
+  }
 };
 
-if (process.env.NODE_ENV !== '') {
-  // production
-  appenders.console = { type: 'console' };
-  categories.default.appenders.push('console');
-  categories.default.level = 'debug';
+if (process.env.NODE_ENV === 'development') {
+  // categories.default.level = 'debug';
 }
 
 log4js.configure({ categories, appenders });
