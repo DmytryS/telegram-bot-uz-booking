f6aa75101528054179e50dfbe18e02ef1c5714e9
Dmytry Shvaika <shvajka@gmail.com>
2019-08-20 01:03:15 +0300

    fix

--- bot/src/lib/jobHandler.js
+++ bot/src/lib/jobHandler.js
@@ -6,60 +6,62 @@ import messages from '../assets/messages/index.js'
 import { print } from '../utils/index.js'
 import { inspect } from 'util'
 
-export const jobHandler = bot => async message => {
-  try {
-    logger.info(`[JobHandler] Received message: ${inspect(message, { colors: true, depth: 4 })}`)
-    let { jobId, type } = JSON.parse(message)
+export default function (bot) {
+  return async message => {
+    try {
+      logger.info(`[JobHandler] Received message: ${inspect(message, { colors: true, depth: 4 })}`)
+      let { jobId, type } = JSON.parse(message)
 
-    const job = await Job.findById(jobId).populate('user')
-    const uzClient = new UzClient.ApiV2(job.user.language)
+      const job = await Job.findById(jobId).populate('user')
+      const uzClient = new UzClient.ApiV2(job.user.language)
 
-    let botMessage = false
-    let response
-    let trains
+      let botMessage = false
+      let response
+      let trains
 
-    switch (type) {
-      case 'FAILED':
-      case 'EXPIRATION':
-        botMessage = messages[job.user.language].watcherDidnotFoundTicket
-        break
-      case 'FOUND':
-        response = await uzClient.Train.find(
-          job.departureStationId,
-          job.arrivalStationId,
-          moment(job.departureDate).format('YYYY-MM-DD'),
-          '00:00:00'
-        )
-
-        if (
-          !response ||
-          (response.data.data && !response.data.data.trains)
-        ) {
-          throw new Error(JSON.stringify(response.data.data))
-        }
-
-        trains = response.data.data.trains.filter(
-          train => train.wagon_types.length > 0
-        )
-
-        if (trains.length > 0) {
-          botMessage = messages[job.user.language].watcherFoundTicket + '\n'
-          botMessage += print.printTrainsList(
-            trains,
+      switch (type) {
+        case 'FAILED':
+        case 'EXPIRATION':
+          botMessage = messages[job.user.language].watcherDidnotFoundTicket
+          break
+        case 'FOUND':
+          response = await uzClient.Train.find(
+            job.departureStationId,
+            job.arrivalStationId,
             moment(job.departureDate).format('YYYY-MM-DD'),
-            job.user.language
+            '00:00:00'
           )
-        }
-        break
-      default:
-        logger.error(`[JobHandler] Unexpected data ${inspect(message, { colors: true, depth: 4 })}`)
-        break
-    }
 
-    if (botMessage) {
-      bot.telegram.sendMessage(job.chatId, botMessage)
+          if (
+            !response ||
+            (response.data.data && !response.data.data.trains)
+          ) {
+            throw new Error(JSON.stringify(response.data.data))
+          }
+
+          trains = response.data.data.trains.filter(
+            train => train.wagon_types.length > 0
+          )
+
+          if (trains.length > 0) {
+            botMessage = messages[job.user.language].watcherFoundTicket + '\n'
+            botMessage += print.printTrainsList(
+              trains,
+              moment(job.departureDate).format('YYYY-MM-DD'),
+              job.user.language
+            )
+          }
+          break
+        default:
+          logger.error(`[JobHandler] Unexpected data ${inspect(message, { colors: true, depth: 4 })}`)
+          break
+      }
+
+      if (botMessage) {
+        bot.telegram.sendMessage(job.chatId, botMessage)
+      }
+    } catch (error) {
+      logger.error(`[JobHandler] Error:${inspect(error, { colors: true, depth: 4 })}`)
     }
-  } catch (error) {
-    logger.error(`[JobHandler] Error:${inspect(error, { colors: true, depth: 4 })}`)
   }
 }
