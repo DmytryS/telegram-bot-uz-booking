471c982bb7123252f3cb90219e2e196e6b87856b
Dmytry Shvaika <shvajka@gmail.com>
2019-08-20 00:57:31 +0300

    fix

--- bot/src/lib/index.js
+++ bot/src/lib/index.js
@@ -1,6 +1,9 @@
 export { default as middlewares } from './middlewares.js'
-export { default as jobHandler } from './jobHandler.js'
 export { default as mongo } from './mongo.js'
 export { default as logger } from './logger.js'
+
 import * as amqp2 from './amqp.js'
 export const amqp = amqp2
+
+import * as jobHandler2 from './jobHandler.js'
+export const jobHandler = jobHandler2
