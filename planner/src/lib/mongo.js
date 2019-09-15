52e94af58a5c655c023c9efdb374135770d49baf
Dmytro Shvaika <dima@Dmytros-MacBook-Pro.local>
2019-09-15 13:26:39 +0300

    mongodb auto reconnect

--- planner/src/lib/mongo.js
+++ planner/src/lib/mongo.js
@@ -1,16 +1,23 @@
 import mongoose from 'mongoose'
 import logger from './logger.js'
 
+const { MONGODB_URI } = process.env
 const options = {
+  useNewUrlParser: true,
   useCreateIndex: true,
-  useNewUrlParser: true
+  autoIndex: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  bufferMaxEntries: 0,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
 }
 
 mongoose.Promise = Promise
 
 mongoose.set('useFindAndModify', false)
 
-mongoose.connect(process.env.MONGODB_URI, options)
+mongoose.connect(MONGODB_URI, options)
 
 mongoose.connection.on('connected', () => {
   logger.info('[MONGO] Connected to MongoDB')
@@ -20,8 +27,12 @@ mongoose.connection.on('error', error => {
   logger.error(`[MONGO] Connection to MongoDB failed: ${error.message}`)
 })
 
+mongoose.connection.on('reconnected', () => {
+  logger.error(`[MONGO] Reconnected to ${MONGODB_URI}`)
+});
+
 mongoose.connection.on('disconnected', () => {
-  logger.info(`[MONGO] Disconnected from ${process.env.MONGODB_URI}`)
+  logger.info(`[MONGO] Disconnected from ${MONGODB_URI}`)
 })
 
 mongoose.connection.on('close', () => {
